3c58d4560e63e7c4c4436df3394f73059a8ad748
n1ki7a <s2pakov@mail.ru>
2023-11-07 09:44:05 +0300

    no message

---
+++ currency_bot/main.py
@@ -0,0 +1,37 @@
+import requests
+import telebot
+
+
+TOKEN = ''
+
+
+bot = telebot.TeleBot(TOKEN)
+
+keys = {
+    'доллар': 'USD',
+    'рубль': 'RUB'
+}
+
+
+@bot.message_handler(commands=['start', 'help'])
+def command_help(message: telebot.types.Message):
+    text = '''Чтобы начать работу введите команду боту в следующем формате:
+<имя валюты> <в какую валюту перевести> <количество>'''
+    bot.reply_to(message, text)
+
+
+@bot.message_handler(commands=['values'])
+def command_values(message: telebot.types.Message):
+    text = 'Доступные валюты:'
+    for key in keys.keys():
+        text = '\n'.join((text, key, ))
+    bot.reply_to(message, text)
+
+
+@bot.message_handler(content_types=['text'])
+def convert(message: telebot.types.Message):
+    queue, base, amount = message.text.split(' ')
+    r = requests.get('https://www.cbr-xml-daily.ru/latest.js')
+
+
+bot.polling()
