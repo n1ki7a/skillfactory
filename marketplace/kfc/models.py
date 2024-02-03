758d43c3cc127a2e542396415218ce3e013044a8
n1ki7a <s2pakov@mail.ru>
2024-02-03 13:16:49 +0300

    Описание моделей

---
+++ marketplace/kfc/models.py
@@ -0,0 +1,57 @@
+from datetime import datetime, timezone
+
+from django.db import models
+from kfc.resources import POSITIONS, cashier
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    price = models.FloatField(default=0.0)
+
+
+class Staff(models.Model):
+    full_name = models.CharField(max_length=255)
+    position = models.CharField(max_length=2,
+                                choices=POSITIONS,
+                                default=cashier)
+    labor_contract = models.IntegerField(max_length=255)
+
+
+class Order(models.Model):
+    time_in = models.DateTimeField(auto_now_add=True)
+    time_out = models.DateTimeField(null=True)
+    cost = models.FloatField(default=0.0)
+    pickup = models.BooleanField(default=False)
+    complete = models.BooleanField(default=False)
+    staff = models.ForeignKey(Staff, on_delete=models.CASCADE)
+    products = models.ManyToManyField(Product, through='ProductOrder')
+
+    def finish_order(self):
+        self.time_out = datetime.now()
+        self.complete = True
+        self.save()
+
+    def get_duration(self):
+        if self.complete:  # если завершён, возвращаем разность объектов
+            return (self.time_out - self.time_in).total_seconds() // 60
+        else:  # если ещё нет, то сколько длится выполнение
+            return (datetime.now(timezone.utc) - self.time_in).total_seconds() // 60
+
+
+class ProductOrder(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+    _amount = models.IntegerField(default=1, db_column='amount')
+
+    @property
+    def amount(self):
+        return self._amount
+
+    @amount.setter
+    def amount(self, value):
+        self._amount = int(value) if value >= 0 else 0
+        self.save()
+
+    def product_sum(self):
+        product_price = self.product.price
+        return product_price * self.amount
