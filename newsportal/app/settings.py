2ad2778a1574ca5661e66ad4c8bab0745cf514c8
n1ki7a <s2pakov@mail.ru>
2024-02-19 17:07:29 +0300

    Фильтр цензор

--- newsportal/app/settings.py
+++ newsportal/app/settings.py
@@ -42,6 +42,8 @@ INSTALLED_APPS = [
     'newsportal',
 ]
 
+SITE_ID = 1
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
--- newsportal/app/urls.py
+++ newsportal/app/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('news/', include('newsportal.urls')),
+    path('', include('newsportal.urls')),
 ]
